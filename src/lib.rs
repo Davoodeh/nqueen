e1635fce2d05c6c1a86f783863464961e1a7e780
M. Yas. Davoodeh <30480116+Davoodeh@users.noreply.github.com>
2022-11-03 06:15:21 +0330

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,280 @@
+//! The library holding implementation free structs of N-Queen.
+
+// TODO replace prints with logs
+
+use rand::prelude::*;
+use std::fmt::Display;
+
+const ALREADY_FILLED_POINT_ERROR: &str = "The point is already taken.";
+
+/// A point on a chess board.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Point {
+    pub row: usize,
+    pub col: usize,
+}
+
+impl Point {
+    pub fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+
+    /// Create a random point on an `n^2` square.
+    pub fn random(n: usize) -> Self {
+        let mut rng = thread_rng();
+        Self {
+            row: rng.gen_range(0..n),
+            col: rng.gen_range(0..n),
+        }
+    }
+}
+
+impl TryInto<(i32, i32)> for Point {
+    type Error = <i32 as TryFrom<usize>>::Error;
+
+    fn try_into(self) -> Result<(i32, i32), Self::Error> {
+        (&self).try_into()
+    }
+}
+
+impl TryInto<(i32, i32)> for &Point {
+    type Error = <i32 as TryFrom<usize>>::Error;
+
+    fn try_into(self) -> Result<(i32, i32), Self::Error> {
+        Ok((self.row.try_into()?, self.col.try_into()?))
+    }
+}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}x{})", self.row + 1, self.col + 1)
+    }
+}
+
+/// Represents an `n*n` chess board.
+#[derive(Debug, Clone)]
+pub struct Board {
+    /// The coordinates of the queens on the board.
+    queens: Vec<Point>,
+    /// Size of the chessboard (usually one wants to solve for the same number of queens).
+    n: usize,
+    /// Caches the relation of checked pieces (gets updated by `mov`, `place` and `capture`).
+    check_data: Vec<Vec<Point>>,
+    /// Maximum number of checks possible for this configuration.
+    max_checks: usize,
+}
+
+impl Board {
+    pub fn new(n: usize) -> Self {
+        let mut candidate = Self {
+            queens: vec![],
+            n,
+            check_data: vec![],
+            max_checks: 0,
+        };
+
+        // place N queens randomly on the board.
+        let mut placed_queens = 0;
+        while placed_queens != n {
+            if candidate.place(&Point::random(n)).is_ok() {
+                placed_queens += 1;
+            }
+        }
+
+        println!("Created the board");
+        println!("Queens: {}", candidate.queens_display());
+        println!("{}", candidate);
+
+        candidate
+    }
+
+    pub fn queens_display(&self) -> String {
+        let mut s = String::new();
+        const SEP: &str = ", ";
+        let n = self.queens.len();
+        s += "[";
+        for i in 0..n {
+            s += &self.queens[i].to_string();
+            if i != (n - 1) {
+                s += SEP;
+            }
+        }
+        s += "]";
+        s
+    }
+
+    /// A getter for the check data.
+    pub fn check_data(&self) -> &Vec<Vec<Point>> {
+        &self.check_data
+    }
+
+    /// A getter for the max checks.
+    pub fn max_checks(&self) -> usize {
+        self.max_checks
+    }
+
+    /// Place a Queen on a given point.
+    pub fn place(&mut self, point: &Point) -> Result<(), &'static str> {
+        if self.queens.contains(point) {
+            Err(ALREADY_FILLED_POINT_ERROR)
+        } else {
+            self.queens.push(point.clone());
+            self.update_check_data();
+            Ok(())
+        }
+    }
+
+    /// Removes a Queen from the game.
+    pub fn capture(&mut self, point: &Point) {
+        self.queens.remove(self.index_of(point).unwrap());
+        self.update_check_data();
+    }
+
+    /// Move a Queen to another position.
+    pub fn mov(&mut self, from: &Point, to: &Point) -> Result<(), &'static str> {
+        if self.queens.contains(to) {
+            Err(ALREADY_FILLED_POINT_ERROR)
+        } else {
+            self.queens.push(to.clone());
+            self.queens.remove(self.index_of(from).unwrap());
+            self.update_check_data();
+            Ok(())
+        }
+    }
+
+    /// Are two of the Queens checking (threatening) each other (counts colat or going thro others).
+    pub fn checking(queen1: &Point, queen2: &Point) -> bool {
+        if queen1 == queen2 {
+            return false;
+        }
+        let (x1, y1): (i32, i32) = queen1.try_into().unwrap();
+        let (x2, y2): (i32, i32) = queen2.try_into().unwrap();
+        x1 == x2 || y1 == y2 || (x1 - x2).abs() == (y1 - y2).abs() /* diagonal */
+    }
+
+    /// Update the list of all the Queens that are checking each other.
+    ///
+    /// If this is used as the heuristic, the furthest away from the answer is the number of edges
+    /// on a Komplete graph: `(self.queens.len() * self.queens.len().saturating_sub(1)) / 2`.
+    ///
+    /// This function counts each threat 2 times, once from the point of view
+    fn update_check_data(&mut self) {
+        let mut v = Vec::<Vec<Point>>::new();
+        // For each unique relation check if two are checking, if yes add them to the list.
+        let n = self.queens.len();
+        for i in 0..n {
+            let mut threats = Vec::<Point>::new();
+            for j in 0..n {
+                let queen1 = &self.queens[i];
+                let queen2 = &self.queens[j];
+                if Self::checking(queen1, queen2) {
+                    threats.push(queen2.clone());
+                }
+            }
+            v.push(threats);
+        }
+        self.check_data = v;
+        self.max_checks = (self.queens.len() * self.queens.len().saturating_sub(1)) / 2;
+    }
+
+    pub fn random_point(&self) -> Point {
+        Point::random(self.n)
+    }
+
+    /// Return the index of the queen which is under the most threat.
+    ///
+    /// Returns nothing if there is no queen on the board.
+    pub fn most_checked(&self) -> Option<usize> {
+        // take out the original index and the data to sort them and be able to track the movements
+        let mut check_data = self
+            .check_data
+            .iter()
+            .enumerate()
+            .map(|(i, v)| (i, v.clone()))
+            .collect::<Vec<(usize, Vec<Point>)>>();
+        // sort by the number of threats
+        check_data.sort_by(|a, b| a.1.len().partial_cmp(&b.1.len()).unwrap());
+        match check_data.last() {
+            Some(v) => Some(v.1.len()),
+            None => None,
+        }
+    }
+
+    /// Move the most threatened queen to another place.
+    ///
+    /// Returns the source and destination.
+    ///
+    /// # Caveats
+    /// - Pancis if called on a board with no queens.
+    /// - Loops forever if the board is filled as it places the queen randomly.
+    pub fn move_most_checked(&mut self) -> (Point, Point) {
+        let most_checked_index = self
+            .most_checked()
+            .expect("Place a queen on the board before trying to move the most checked");
+        let src = self.queens[most_checked_index].clone();
+        let mut dest = self.random_point();
+        loop {
+            if self.mov(&src, &dest).is_ok() {
+                break;
+            }
+            dest = self.random_point();
+        }
+        (src, dest)
+    }
+
+    /// Search for a queen at a certain position.
+    pub fn index_of(&self, point: &Point) -> Option<usize> {
+        self.queens.iter().position(|p| *p == *point)
+    }
+}
+
+impl Display for Board {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let n = self.n;
+
+        const FILLER: usize = 3; // An odd number! (count of empty spaces between each piece)
+
+        let div = |filler: &str, sep: &str| {
+            (filler.repeat(FILLER) + sep).repeat(n - 1) + &filler.repeat(FILLER)
+        };
+        // empty spaces enough for filling a around a symbol
+        let fill = &" ".repeat(FILLER);
+        let half_fill = &" ".repeat(FILLER / 2);
+        let filled = |i: &str| format!("{0}{1}{0}", half_fill, i);
+
+        let col_counter = &filled(&{
+            let mut s = String::new();
+            for i in 1..=n {
+                s += " "; // for col separator bars |
+                s += &filled(&(i % 10).to_string());
+            }
+            s += half_fill;
+            s += "  \n";
+            s
+        });
+
+        write!(f, " {}", col_counter)?;
+        write!(f, "{0}╔{1}╗{0} \n", half_fill, div("═", "╦"))?;
+        for i in 0..n {
+            let mut row = String::new();
+            for j in 0..n {
+                match self.index_of(&Point::new(i, j)) {
+                    Some(i) => row += &filled(&self.check_data[i].len().to_string()),
+                    None => row += fill,
+                }
+
+                if j != (n - 1) {
+                    row += "│";
+                }
+            }
+
+            write!(f, "{0} ║{1}║ {0}\n", (i + 1) % 10, row)?;
+            if i != (n - 1) {
+                write!(f, "  ╠{}╣  \n", div("─", "┼"))?;
+            }
+        }
+        write!(f, "{0} ╚{1}╝ {0}\n", half_fill, div("═", "╩"))?;
+        write!(f, " {}", col_counter)?;
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+//! N-Queen problem solution with heuristic ways.
+//!
+//! This implementation implements a heuristic function based on the number of threats queens pose
+//! to each other (includes indirect threats through each other).
+//!
+//! The implementation runs a VERY greedy search which tries to move the most threatened peice `n`
+//! times only if lowers the heuristic. If after a `z` number of tries (defined in
+//! `lower_heuristic`) fails to do so, the program panics.
+//!
+//! This is a homework example of a "heuristic function implementation" not an attempt to solve the
+//! N-Queens.
+
+use nqueen::{Board, Point};
+
+/// The estimated distance to the anwser (greater = further).
+fn heuristic(board: &Board) -> usize {
+    board.check_data().iter().map(|i| i.len()).sum::<usize>() / 2
+}
+
+/// Move a piece the most checked only if the heuristic shows a lower value.
+///
+/// Breaks after fixed number of attempts.
+fn lower_heuristic(board: &mut Board) -> Result<(usize, usize, Point, Point), &'static str> {
+    const MAX_ATTEMPTS: usize = 1000000;
+    for _ in 0..MAX_ATTEMPTS {
+        let pre_h = heuristic(board);
+        let (from, to) = board.move_most_checked();
+        let post_h = heuristic(board);
+        if pre_h < post_h {
+            board.mov(&to, &from).unwrap();
+        } else {
+            return Ok((pre_h, post_h, from, to));
+        }
+    }
+    Err("Got stuck in a local minima")
+}
+
+fn main() {
+    let n = std::env::args()
+        .nth(1)
+        .expect("Expected an input as the number of Queens (N)")
+        .to_owned()
+        .parse::<usize>()
+        .expect("Invalid number as the input.");
+
+    if n < 4 {
+        panic!("Given number must be bigger than 4");
+    }
+
+    let mut board = Board::new(n);
+    let max_h = board.max_checks();
+    println!("Initial heuristic: {}/{}", heuristic(&board), max_h);
+
+    const MAX_MOVES: usize = 100000;
+    println!("Not printing random moves without a heustiric change");
+    for i in 0..MAX_MOVES {
+        let pre_move = board.to_string();
+        let (pre_h, h, from, to) = lower_heuristic(&mut board)
+            .expect("Expected to lower the heuristic by playing moving the most checked queen");
+        let progress = pre_h - h;
+        if progress > 0 {
+            println!(
+                "Move #{}: the most checked queen {} -> {} (benefit: -{}h, total: {}h)",
+                i, from, to, progress, h
+            );
+            let post_move = board.to_string();
+            let pre = pre_move.split("\n").collect::<Vec<&str>>();
+            let post = post_move.split("\n").collect::<Vec<&str>>();
+
+            for i in 0..(pre.len() - 1)
+            /* last line is empty */
+            {
+                println!("{}  -->  {}", pre[i], post[i]);
+            }
+            println!("\n{}\n", "#".repeat(79));
+        }
+        if h == 0 {
+            println!("{}", board);
+            println!("SOLVED!");
+            break;
+        }
+    }
+}
